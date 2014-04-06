c9e33160946088ceea9f7e2c7c6a35138b328290
Kai-Yuan Neo <kaiyuan.neo@gmail.com>
2014-04-06 14:10:31 -0700

    testing form action

--- confirmation.php
+++ confirmation.php
@@ -1,19 +1,29 @@
-<?php 
-  require 'PHPMailerAutoload.php'
-  // Check if the "from" input field is filled out
-  echo "Thank you for sending us feedback";
-  if (isset($_POST["email"]))
-    {
-    $dest = "kaiyuan.neo@gmail.com";
-    $subject = "[NEW SIGNUP] New member company";
-    $message = $_POST["comments"];
-    // message lines should not exceed 70 characters (PHP rule), so wrap it
-    $message = wordwrap($message, 70);
-    $from = $_POST["email"]; // sender
-    // send mail
-    mail("kaiyuan.neo@gmail.com", $subject, $message, null, -f$from);
-    }
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SF Citi Registration</title>
 
-<script> console.log('hello'); </script>
-<a href="http://svangel.com">SV Angel Homepage</a>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php 
+      require 'PHPMailerAutoload.php'
+      // Check if the "from" input field is filled out
+      echo "Thank you for sending us feedback";
+    ?>
+    <script> console.log('hello'); </script>
+    <a href="http://svangel.com">SV Angel Homepage</a>
+  </body>
+</html>
