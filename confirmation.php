857f90ef9ff4ab81d3ed967b498a90259a49f48e
Kai-Yuan Neo <kaiyuan.neo@gmail.com>
2014-04-06 14:22:03 -0700

    testing form action

---
+++ confirmation.php
@@ -0,0 +1,7 @@
+<?php 
+  require 'PHPMailerAutoload.php'
+  // Check if the "from" input field is filled out
+  echo "Thank you for sending us feedback";
+?>
+<script> console.log('hello'); </script>
+<a href="http://svangel.com">SV Angel Homepage</a>
